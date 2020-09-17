baacc35f539bf6b0afd45b614ad69aa3145b7f01
Waffle <waffle.lapkin@gmail.com>
2020-09-17 15:55:29 +0300

    skip all commits which author is not `bors`

--- src/main.rs
+++ src/main.rs
@@ -97,6 +97,17 @@ async fn pull(
     let mut opts = DiffOptions::default();
     let opts = opts.context_lines(0).minimal(true);
     for [prev, next] in commits?.array_windows::<[_; 2]>() {
+        if next.author().name() != Some("bors") {
+            log::warn!(
+                "Skip commit#{} from non-bors user@{}: {}",
+                next.id(),
+                next.author().name().unwrap_or("<invalid utf-8>"),
+                next.message().unwrap_or("<invalid utf-8>").trim_end_matches('\n'),
+            );
+
+            continue;
+        }
+
         let diff: Diff =
             repo.diff_tree_to_tree(Some(&prev.tree()?), Some(&next.tree()?), Some(opts))?;
         let (krate, action) = diff_one(diff)?;
